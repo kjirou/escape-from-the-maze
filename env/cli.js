d1a04cf92db1d064d50b1ef48011db85f2293d2a
kjirou <kjirou.web@gmail.com>
2015-08-31 01:16:33 +0900

    Fix a bug

--- env/cli.js
+++ env/cli.js
@@ -1,4 +1,7 @@
 process.env.NODE_ENV = 'cli';
 process.env.NODE_PATH = __dirname + '/..';
 require('module')._initPaths();
-require('babel/register');
+require('babel/register')({
+  ignore: false,
+  extensions: ['.es6']
+});
